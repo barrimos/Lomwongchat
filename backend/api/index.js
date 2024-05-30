141949da9fc30a0a856dac407f2ac9d72e14446e
Prapas Khong-attagarn <prapas.hkr@gmail.com>
2024-05-31 02:46:05 +0700

    v1 disabled socket

--- backend/api/index.js
+++ backend/api/index.js
@@ -27,20 +27,20 @@ const channels = {
 const usersJoined = ['']
 
 const options = {
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://lomwongchat.vercel.app/'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://lomwongchat.vercel.app'],
   methods: 'GET, POST, DELETE',
   credentials: true
 }
 
 const app = express()
-const server = http.createServer(app)
-const io = socket_io(server, {
-  cors: {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://lomwongchat.vercel.app/'],
-    methods: ['GET', 'POST', 'DELETE'],
-    credentials: true
-  }
-})
+// const server = http.createServer(app)
+// const io = socket_io(server, {
+//   cors: {
+//     origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://lomwongchat.vercel.app'],
+//     methods: ['GET', 'POST', 'DELETE'],
+//     credentials: true
+//   }
+// })
 
 app.use(cors(options))
 app.use(cookieParser())
@@ -68,93 +68,93 @@ app.use('*', (req, res, next) => {
   res.status(404).json({ error: 'You Lost' })
 })
 
-io.on('connection', socket => {
-  console.log(`${socket.id} connected`)
+// io.on('connection', socket => {
+//   console.log(`${socket.id} connected`)
 
-  socket.on('verify', username => {
-    if (username === 'admin') {
-      adminConnected[username] = socket.id
-    } else {
-      clientsConnected[username] = socket.id
-    }
-    loginName[socket.id] = username
+//   socket.on('verify', username => {
+//     if (username === 'admin') {
+//       adminConnected[username] = socket.id
+//     } else {
+//       clientsConnected[username] = socket.id
+//     }
+//     loginName[socket.id] = username
 
-    socket.emit('getId', socket.id)
-    io.emit('usersOnline', Object.keys(clientsConnected)) // send to client
+//     socket.emit('getId', socket.id)
+//     io.emit('usersOnline', Object.keys(clientsConnected)) // send to client
 
-    // initial join
-    socket.join('lobby')
-  })
+//     // initial join
+//     socket.join('lobby')
+//   })
 
-  socket.on('joinChannel', data => {
-    if (!channels[data[1]] && data[1]) {
-      channels[data[1]] = { count: 0, users: [] }
-    }
+//   socket.on('joinChannel', data => {
+//     if (!channels[data[1]] && data[1]) {
+//       channels[data[1]] = { count: 0, users: [] }
+//     }
 
-    if (data[0]) {
-      channels[data[0]].count--
-      channels[data[0]].users.splice(channels[data[0]].users.indexOf(data[2]), 1)
-      socket.leave(data[0])
-    }
-    if (data[1]) {
-      channels[data[1]].count++
-      channels[data[1]].users.push(data[2])
-      socket.join(data[1])
-      socket.to(adminConnected['admin']).emit('usersJoinedChannel', data)
-      usersJoined[1] = data[1]
-      usersJoined[2] = data[2]
-    }
-  })
-  socket.on('toD', () => {
-    if (usersJoined[1] && usersJoined[2]) socket.emit('usersJoinedChannel', usersJoined)
-  })
+//     if (data[0]) {
+//       channels[data[0]].count--
+//       channels[data[0]].users.splice(channels[data[0]].users.indexOf(data[2]), 1)
+//       socket.leave(data[0])
+//     }
+//     if (data[1]) {
+//       channels[data[1]].count++
+//       channels[data[1]].users.push(data[2])
+//       socket.join(data[1])
+//       socket.to(adminConnected['admin']).emit('usersJoinedChannel', data)
+//       usersJoined[1] = data[1]
+//       usersJoined[2] = data[2]
+//     }
+//   })
+//   socket.on('toD', () => {
+//     if (usersJoined[1] && usersJoined[2]) socket.emit('usersJoinedChannel', usersJoined)
+//   })
 
-  socket.on('message', ({ rid, username, message, rawTime, timestamp, reply }) => {
-    const data = {
-      username: username,
-      message: message,
-      rawTime: rawTime,
-      timestamp: timestamp,
-      reply: reply
-    }
+//   socket.on('message', ({ rid, username, message, rawTime, timestamp, reply }) => {
+//     const data = {
+//       username: username,
+//       message: message,
+//       rawTime: rawTime,
+//       timestamp: timestamp,
+//       reply: reply
+//     }
 
-    io.to(rid ?? 'lobby').emit('broadcast', data)
-  })
+//     io.to(rid ?? 'lobby').emit('broadcast', data)
+//   })
 
-  socket.on('newChannelCreated', newChannelName => {
-    channels[newChannelName] = { count: 0, users: [] }
-    io.emit('fetchNewChannel')
-  })
+//   socket.on('newChannelCreated', newChannelName => {
+//     channels[newChannelName] = { count: 0, users: [] }
+//     io.emit('fetchNewChannel')
+//   })
 
-  socket.on('logout', (username, skid, channel) => {
-    channels[channel].users.splice(channels[channel].users.indexOf(loginName[skid]), 1)
-    channels[channel].count--
-    if (username !== 'admin') {
-      socket.to(adminConnected['admin']).emit('usersJoinedChannel', usersJoined, true)
-    }
-    usersJoined[1] = ''
-    usersJoined[2] = ''
-    delete adminConnected[username]
-    delete clientsConnected[username]
-    delete loginName[skid]
+//   socket.on('logout', (username, skid, channel) => {
+//     channels[channel].users.splice(channels[channel].users.indexOf(loginName[skid]), 1)
+//     channels[channel].count--
+//     if (username !== 'admin') {
+//       socket.to(adminConnected['admin']).emit('usersJoinedChannel', usersJoined, true)
+//     }
+//     usersJoined[1] = ''
+//     usersJoined[2] = ''
+//     delete adminConnected[username]
+//     delete clientsConnected[username]
+//     delete loginName[skid]
 
-    io.emit('usersOnline', Object.keys(clientsConnected))
-  })
+//     io.emit('usersOnline', Object.keys(clientsConnected))
+//   })
 
-  socket.on('beforeUnload', (skid, channel) => {
-    channels[channel].users.splice(channels[channel].users.indexOf(loginName[skid]), 1)
-    if (channels[channel].count > 0) channels[channel].count--
-    if (loginName[skid] === 'admin') {
-      delete adminConnected[loginName[skid]]
-    } else {
-      delete clientsConnected[loginName[skid]]
-    }
-    delete loginName[skid]
-  })
+//   socket.on('beforeUnload', (skid, channel) => {
+//     channels[channel].users.splice(channels[channel].users.indexOf(loginName[skid]), 1)
+//     if (channels[channel].count > 0) channels[channel].count--
+//     if (loginName[skid] === 'admin') {
+//       delete adminConnected[loginName[skid]]
+//     } else {
+//       delete clientsConnected[loginName[skid]]
+//     }
+//     delete loginName[skid]
+//   })
 
-  socket.on('disconnect', () => {
-    console.log(`${socket.id} disconnect`)
-  })
-})
+//   socket.on('disconnect', () => {
+//     console.log(`${socket.id} disconnect`)
+//   })
+// })
 
-module.exports = server
+module.exports = app
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
-const server = require('./api/index')
+const app = require('./api/index')
 const config = require('./config')
 
 
 const boot = async () => {
   // await mongoose.connect('mongodb://localhost:27017/lomwongchat')
   await mongoose.connect(config.mongoUri, config.mongoOptions)
-  server.listen(config.port, () => {
+  app.listen(config.port, () => {
     console.log(`server listening on ${config.port}`)
   })
 }
